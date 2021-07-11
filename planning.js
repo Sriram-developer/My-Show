ff8273973f61564119601856fac2c0e3d9a11f66
SRIRAM <Sriram>
2021-07-11 17:44:29 +0530

    add route & HOC files

---
+++ planning.js
@@ -0,0 +1,21 @@
+//Book My Show Clone Planning 
+
+// # We need a reusable carousel #
+// *Hero carousel
+// *Common carousel
+// *Cast and Crew small carousel
+
+//Pages
+
+// *Home page 
+// *Movie page
+// *Categories page 
+// *Authetication page
+// *Razorpay payment gateway
+
+// How ?
+
+// Layouts
+// Routes
+// Pages
+// Components
---
+++ src/HOC/Default.HOC.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const DefaultHOC = () => {
+    return (<div></div>);
+};
+
+export default DefaultHOC;
+
