df7c9586a3cef153c1858acc22231751bf34fc4b
SRIRAM <Sriram>
2021-07-17 15:37:00 +0530

    add Hero carousal

--- src/components/HeroCarousal/HeroCarousal.component.jsx
+++ src/components/HeroCarousal/HeroCarousal.component.jsx
@@ -7,10 +7,19 @@ import "slick-carousel/slick/slick-theme.css";
 
 const HeroCarousal = () => {
 
+
+ const settingsLG ={arrow: true, 
+    autoplay:true,
+    centerMode: true,
+    centerPadding: "300px",
+    slidesToShow:1,
+    infinite: true,
+    slidesToScroll:1,
+  };         
+           
+           
 const settings = {
     arrows:true,
-    centerMode:true,
-    centerpadding:"200px",
     dots: true,
     infinite: true,
     speed: 500,
@@ -25,22 +34,33 @@ const settings = {
   "https://www.eastcoastdaily.in/wp-content/uploads/2021/05/3-2.jpg",
   "https://www.eastcoastdaily.in/wp-content/uploads/2021/05/3-2.jpg",
   
-
-  ];
+];
   
 
   return (<>
   
+  <div className="lg:hidden">
   <HeroSlider {...settings}>
       {
           images.map((image) =>(
-              <div className="w-20 h-80">
+              <div className="w-full h-56 md:h-80 py-3">
                   <img src={image} alt="test"className="w-full h-full"/>
               </div>
           ))
       }
   </HeroSlider>
 
+  </div>
+  <div className="hidden lg:block"><HeroSlider {...settingsLG}>
+      {
+          images.map((image) =>(
+              <div className="w-full h-96 px-2 py-3">
+                  <img src={image} alt="test"className="w-full h-full rounded-md"/>
+              </div>
+          ))
+      }
+  </HeroSlider>
+</div>
   </>);
 };
 
