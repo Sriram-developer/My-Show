7a37c9976d5570f8aa18581ba4fcd588f8d83f5c
SRIRAM <Sriram>
2021-07-19 15:56:27 +0530

    Add Cast & Crew

--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {FaCcVisa,FaCcApplePay} from "react-icons/fa";
 
 // Component
 import MovieHero from "../components/MovieHero/MovieHero.component";
@@ -7,6 +8,47 @@ const Movie = () => {
     return (
         <>
           <MovieHero/>
+          <div className="my-12 container px-4 lg:ml-20 lg:w-2/3">
+            <div className="flex flex-col items-start gap-3">
+            <h2 className="text-gray-800 font-bold  text-2xl">About the movie</h2>
+            <p>Bruce Wayne and Diana Prince try to bring the metahumans of Earth together after 
+              the death of Clark Kent. Meanwhile,
+              Darkseid sends Steppenwolf to Earth with an army to subjugate humans.</p>
+            </div>
+            <div className="my-8">
+              <hr/>
+            </div>
+
+            <div className="my-8">
+              <h1 className="text-gray-800 font-bold  text-2xl mb-3">Appicable offers</h1>
+
+              <div className="flex flex-col gap-3 lg:flex-row">
+
+             <div className="flex items-start gap-2 bg-yellow-100 p-3  border-yellow-400  border-dashed border-2 rounded-md">
+                <div className="w-8 h-8">
+                <FaCcVisa className="w-full h-full "/>
+                  </div>
+               
+               <div className="flex flex-col items-start">
+                 <h3 className="text-gray-700 text-xl font-bold">Visa Stream Offer</h3>
+                 <p className="text-gray-600">Get 50% off up to INR 150 on all Visa cards* on BookMyShow Stream.</p>
+               </div>
+               </div>
+
+               <div className="flex items-start gap-2 bg-yellow-100 p-3  border-yellow-400  border-dashed border-2 rounded-md">
+                <div className="w-8 h-8">
+                <FaCcApplePay className="w-full h-full "/>
+                  </div>
+               
+               <div className="flex flex-col items-start">
+                 <h3 className="text-gray-700 text-xl font-bold">Filmy Pass</h3>
+                 <p className="text-gray-600">Get Rs.75* off on 3 movies you buylrent on stream.Buy Filmy pass @Rs.99</p>
+               </div>
+               </div>
+              
+              </div>
+            </div>
+          </div>
         </>
     );
 };
