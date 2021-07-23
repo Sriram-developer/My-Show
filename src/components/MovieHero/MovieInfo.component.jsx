26ebd1008988c17699b11b78b04a886b9f668a5f
SRIRAM <Sriram>
2021-07-24 04:22:03 +0530

    Add Razorpay Gateway

--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,15 +1,34 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
+
+// Components
+import PaymentModal from "../PaymentModel/Payment.component";
 
 // context
 import { MovieContext } from "../../context/movie.context";
 
 const MovieInfo = () => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [price, setPrice] = useState(0);
+
     const {movie} = useContext(MovieContext);
      // optional chaining
     const genres = movie.genres?.map(({name}) => name).join(",");
 
+    const rentMovies = () => {
+        setIsOpen(true);
+        setPrice(149);
+    };
+
+
+    const buyMovies = () => {
+        setIsOpen(true);
+        setPrice(399);
+    };
+
+
     return (
         <>
+           <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price}/>
             <div className="flex flex-col gap-3 lg:gap-8">
                 <div className="flex items-center gap-3 md:px-4">
                     <div className="w-40 h-8">
@@ -27,8 +46,8 @@ const MovieInfo = () => {
 
                 </div>
                 <div className="flex items-center gap-3 md:px-4 mx-2 md:w-screen lg:w-full ">
-                    <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Rent ₹149</button>
-                    <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Buy ₹149</button>
+                    <button onClick={rentMovies} className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Rent ₹149</button>
+                    <button onClick={buyMovies}  className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">Buy ₹399</button>
                 </div>
               </div>
             </div>
--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -20,6 +20,9 @@ const Movie = () => {
   const {id} = useParams();
   const {movie} = useContext(MovieContext);
   const [cast, setCast] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+  const [recommended, setRecommended] = useState([]);
+
 
   useEffect(() => {
     const requestCast = async() => {
@@ -27,28 +30,28 @@ const Movie = () => {
       setCast(getCast.data.cast);
     };
     requestCast();
-  },[]);
+  },[id]);
 
   useEffect(() => {
-    const requestTopRatedMovies = async () => {
-       const getTopRatedMovies = await axios.get("/movie/top_rated");
-       setTopRatedMovies(getTopRatedMovies.data.results);
+    const requestSimilarMovies = async () => {
+       const getSimilarMovies = await axios.get(`/movie/${id}/similar`);
+       setSimilarMovies(getSimilarMovies.data.results);
     };
 
-    requestTopRatedMovies();
+    requestSimilarMovies();
 
-  }, []);
+  }, [id]);
 
 
   useEffect(() => {
-    const requestTopRatedMovies = async () => {
-       const getTopRatedMovies = await axios.get("/movie/top_rated");
-       setTopRatedMovies(getTopRatedMovies.data.results);
+    const requestRecommendedMovies = async () => {
+       const getRecommendedMovies = await axios.get(`/movie/${id}/recommendations`);
+       setRecommended(getRecommendedMovies.data.results);
     };
 
-    requestTopRatedMovies();
+    requestRecommendedMovies();
 
-  }, []);
+  }, [id]);
 
   
   const settings = {
@@ -182,11 +185,11 @@ const settingsCast = {
               <hr/>
             </div>
             <div className="my-8">
-            <PosterSlider config={settings} images={TempPosters} title="You Might Also Like" isDark={false}/>
+            <PosterSlider config={settings} images={similarMovies} title="You Might Also Like" isDark={false}/>
             <div className="my-8">
               <hr/>
             </div>
-            <PosterSlider config={settings} images={TempPosters} title="BMS XCLUSIVE" isDark={false}/>
+            <PosterSlider config={settings} images={recommended} title="BMS XCLUSIVE" isDark={false}/>
             </div>
           </div>
         </>
